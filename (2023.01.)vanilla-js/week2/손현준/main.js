505b01d89043a25699be05f1b51103f47865bbec
SHJcompany <93332615+SHJcompany@users.noreply.github.com>
2023-01-07 20:23:35 +0900

    Login

--- (2023.01.)vanilla-js/week2/손현준/main.js
+++ (2023.01.)vanilla-js/week2/손현준/main.js
@@ -1,5 +1,5 @@
 const h4 = document.querySelector(".SHJ-component__title");
-const h5 = document.querySelector("div.SHJ-component__text:second-child h5")
+const h5 = document.querySelector(".SHJ-component__text2")
 
 function handleTitleMouseEnter() {
     h4.innerText = "환영합니다.";
@@ -13,11 +13,34 @@ h4.addEventListener("mouseenter", handleTitleMouseEnter);
 h4.addEventListener("mouseleave", handleMouseLeave);
 
 function handleTitleClick() {
-    if(h5.className = "SHJ-component__title2"){
+    if(h5.className = "SHJ-component__text2"){
         h5.className = "";
     } else {
-        h5.className = "SHJ-component__title2"
+        h5.className = "SHJ-component__text2"
     };
 }
 
 h5.addEventListener("click", handleTitleClick);
+
+const loginForm = document.querySelector(".SHJ-login-form");
+const loginInput = document.querySelector(".SHJ-login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+    // const username = loginInput.value;
+    // console.log(username);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick)
+
+handleLinkClick()
+
